83c8ca1393099f62aa1b2ef0f796f68af7c50cde
REE-TISH <reetishk798@gmail.com>
2025-08-10 11:45:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter , createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
+import {BrowserRouter , createBrowserRouter, createRoutesFromElements, Route,RouterProvider} from 'react-router-dom'
 
 const router = createBrowserRouter( createRoutesFromElements(
   <Route path="/" element={<App />} />
